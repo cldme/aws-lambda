3f89473dbbc3052c146377f8f49557fa5414172f
cldme <ioncld97@gmail.com>
2020-05-24 15:56:34 +0200

    fix negative stock and user credit (now logging error message when this happens)

--- services/stock_subtract/lambda_function.py
+++ services/stock_subtract/lambda_function.py
@@ -19,18 +19,19 @@ def lambda_handler(event, context):
         stock_object = stock_table.get_item(Key={'id': item_id})
         current_stock = stock_object['Item']['stock']
         new_stock = current_stock - amount
-        if new_stock >= 0:
-            response = stock_table.update_item(
-                Key={'id': item_id},
-                UpdateExpression="set stock = :stock",
-                ExpressionAttributeValues={':stock': decimal.Decimal(new_stock)},
-                ReturnValues="UPDATED_NEW"
-            )
-            res = json.dumps(response, default=str)
-            print(f'stock successfully subtracted: {res}')
-            statusCode = 200
-        else:
-            statusCode = 400
+
+        if new_stock < 0:
+            raise ValueError('stock can not be negative!')
+
+        response = stock_table.update_item(
+            Key={'id': item_id},
+            UpdateExpression="set stock = :stock",
+            ExpressionAttributeValues={':stock': decimal.Decimal(new_stock)},
+            ReturnValues="UPDATED_NEW"
+        )
+        res = json.dumps(response, default=str)
+        print(f'stock successfully subtracted: {res}')
+        statusCode = 200
     except Exception as e:
         print(f'update_item error: {e}')
         statusCode = 400
--- services/users_credit_subtract/lambda_function.py
+++ services/users_credit_subtract/lambda_function.py
@@ -19,18 +19,19 @@ def lambda_handler(event, context):
         user_object = users_table.get_item(Key={'id': user_id})
         current_credit = user_object['Item']['credit']
         new_credit = current_credit - amount
-        if new_credit >= 0:
-            response = users_table.update_item(
-                Key={'id': user_id},
-                UpdateExpression="set credit = :credit",
-                ExpressionAttributeValues={':credit': decimal.Decimal(new_credit)},
-                ReturnValues="UPDATED_NEW"
-            )
-            res = json.dumps(response, default=str)
-            print(f'credit successfully subtracted: {res}')
-            statusCode = 200
-        else:
-            statusCode = 400
+
+        if new_credit < 0:
+            raise ValueError('user credit can not be negative!')
+
+        response = users_table.update_item(
+            Key={'id': user_id},
+            UpdateExpression="set credit = :credit",
+            ExpressionAttributeValues={':credit': decimal.Decimal(new_credit)},
+            ReturnValues="UPDATED_NEW"
+        )
+        res = json.dumps(response, default=str)
+        print(f'credit successfully subtracted: {res}')
+        statusCode = 200
     except Exception as e:
         print(f'update_item error: {e}')
         statusCode = 400
