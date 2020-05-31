d72dd322731a39c2a12537c980b04681a34f03ed
cldme <ioncld97@gmail.com>
2020-05-31 15:10:17 +0200

    cleanup payment and stock update code

--- services/orders/checkout/lambda_function.py
+++ services/orders/checkout/lambda_function.py
@@ -23,6 +23,66 @@ def invoke_lambda(name, payload, invocation_type='RequestResponse'):
 
     return res
 
+# make payment
+def make_payment(order):
+    print(f'making payment via calling the payment service')
+    payload = {
+        'pathParameters': {
+            'user_id': order['user_id'],
+            'order_id': order['id'],
+            'amount': str(order['total_cost'])
+        }
+    }
+    res = invoke_lambda('payment_pay_lambda', payload)
+    # get result object from lambda call
+    res = json.loads(res['Payload'].read())
+    print(f'payment service result: {res}')
+
+    if res['statusCode'] != 200:
+        raise ValueError('error thrown while making the payment')
+
+# subtract stock
+def subtract_stock(order):
+    print(f'subtract stock via calling the stock service')
+    items = order['items']
+    # keep track of the items that were updated (used for rollback in case of failure)
+    queue = list()
+    for item in items:
+        payload = {
+            'pathParameters': {
+                'item_id': item,
+                'number': '1'
+            }
+        }
+        res = invoke_lambda('stock_subtract_lambda', payload)
+        # get result object from lambda call
+        res = json.loads(res['Payload'].read())
+        print(f'stock subtract service result: {res}')
+
+        # in case of failure rollback previous actions
+        if res['statusCode'] != 200:
+            print(f'failed updating stock with result: {res}')
+            print(f'rolling back now!')
+            # rollback user's payment
+            payload = {
+                'pathParameters': {
+                    'user_id': order['user_id'],
+                    'order_id': order['id']
+                }
+            }
+            # invoke async lambda (no need to wait for results here)
+            res = invoke_lambda('payment_cancel_lambda', payload, 'Event')
+            # rollback stock subtractions
+            for q in queue:
+                # invoke async lambda (no need to wait for results here)
+                res = invoke_lambda('stock_add_lambda', q, 'Event')
+            # exit after rollback is complete
+            raise ValueError('error while updating the stock! rollback complete!')
+        
+        print(f'subtracted stock for item: {item} with result: {res}')
+        # in case of success add item to done queue
+        queue.append(payload)
+
 def lambda_handler(event, context):
     
     orders_table = dynamodb.Table(ORDERS_TABLE)
@@ -33,64 +93,12 @@ def lambda_handler(event, context):
         order = res['Item']
 
         # make the payment via calling the payment service
-        print(f'making payment via calling the payment service')
-        payload = {
-            'pathParameters': {
-                'user_id': order['user_id'],
-                'order_id': order_id,
-                'amount': str(order['total_cost'])
-            }
-        }
-        res = invoke_lambda('payment_pay_lambda', payload)
-        # get result object from lambda call
-        res = json.loads(res['Payload'].read())
-        print(f'payment service result: {res}')
+        make_payment(order)
 
-        if res['statusCode'] != 200:
-            raise ValueError('error thrown while making the payment')
-
-        print(f'done making the payment with result: {res}')
+        print(f'done making the payment!')
 
         # subtract the stock via calling the stock service
-        print(f'subtract stock via calling the stock service')
-        items = order['items']
-        # keep track of the items that were updated (used for rollback in case of failure)
-        queue = list()
-        for item in items:
-            payload = {
-                'pathParameters': {
-                    'item_id': item,
-                    'number': '1'
-                }
-            }
-            res = invoke_lambda('stock_subtract_lambda', payload)
-            # get result object from lambda call
-            res = json.loads(res['Payload'].read())
-            print(f'stock subtract service result: {res}')
-
-            # in case of failure rollback previous actions
-            if res['statusCode'] != 200:
-                print(f'failed updating stock with result: {res}')
-                print(f'rolling back now!')
-                # rollback user's payment
-                payload = {
-                    'pathParameters': {
-                        'user_id': order['user_id'],
-                        'order_id': order_id
-                    }
-                }
-                # invoke async lambda (no need to wait for results here)
-                res = invoke_lambda('payment_cancel_lambda', payload, 'Event')
-                # rollback stock subtractions
-                for q in queue:
-                    # invoke async lambda (no need to wait for results here)
-                    res = invoke_lambda('stock_add_lambda', q, 'Event')
-                # exit after rollback is complete
-                raise ValueError('error while updating the stock! rollback complete!')
-            
-            print(f'subtracted stock for item: {item} with result: {res}')
-            # in case of success add item to done queue
-            queue.append(payload)
+        subtract_stock(order)
 
         print(f'done updating all stock!')
 
